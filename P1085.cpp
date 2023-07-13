f3782902808033c44f6131c9f8be2b3f7290bc0d
zipeng <782237557@qq.com>
2023-07-13 20:16:34 +0800

    p1085 ac

--- P1085.cpp
+++ P1085.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
-    cout<<"a"<<endl;
+    int result = 0;
+    int max_unhappy = 0;
+    for(int i=1;i<=7;i++){
+        int school,home;
+        cin>>school>>home;
+        int now_unhappy = school + home;
+        bool is_unhappy = now_unhappy > 8?true:false;
+        if(is_unhappy && now_unhappy > max_unhappy){
+            max_unhappy = now_unhappy;
+            result = i;
+        }
+    }
+    cout<<result<<endl;
     return 0;
 }
