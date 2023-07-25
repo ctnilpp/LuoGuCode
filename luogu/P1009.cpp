e04feeb752dfc2804273cf407e19a39972bb5628
SpeakBird <49940531+ctnilpp@users.noreply.github.com>
2023-07-25 16:19:55 +0800

    Revert "tijiao"
    This reverts commit 2ca10edc0f14a84586a30a9950cc33963158b619.

--- luogu/P1009.cpp
+++ luogu/P1009.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a[500];
-    int b[500];
-    int n;
-    cin>>n;
+    int a[505];
+    int b[505];
+    int c[505];
+    string a1;
+    string b1;
+    cin>>a1;
+    cin>>b1;
+    int max_idx = max(a1.length(), b1.length());
+    for(int i=a1.length()-1,j=0;i>=0;i--,j++){
+        a[j] = a1[i] - '0';
+    }
+    for(int i=b1.length()-1,j=0;i>=0;i--,j++){
+        b[j] = b1[i] - '0';
+    }
+    //注意要填充垃圾数据
+    for(int i=a1.length();i<max_idx;i++){
+        a[i] = 0;
+    }
+    for(int i=b1.length();i<max_idx;i++){
+        b[i] = 0;
+    }
+    c[0] = 0;
+    for(int i=0;i<max_idx;i++){
+        c[i] += a[i]+b[i];
+        c[i+1] = c[i]/10;
+        c[i] %=10;
+    }
 
     return 0;
 }
--- luogu/test2.cpp
+++ luogu/test2.cpp
@@ -1,23 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int score;
-    cin>>score;
-    if(score >= 90){
-        cout<<"A"<<endl;
-    }
-    else if(score >= 80){
-        cout<<"B"<<endl;
-    }
-    else if(score >= 70){
-        cout<<"C"<<endl;
-    }
-    else if(score >= 60){
-        cout<<"D"<<endl;
-    }
-    else{
-        cout<<"²»¼°¸ñ"<<endl;
-    }
+    char mingzi;
+    cin>>mingzi;
+    int num = (int)mingzi;
+    char result = num - 32;
+    cout<<result<<endl;
     return 0;
 }
 
